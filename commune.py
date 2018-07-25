412e70329ab4d3d40c0e76eab24fab83a82a32ee
HelmiHalmouch <helmirealmadrid@gmail.com>
2018-07-25 02:36:52 +0200

    def the function to returun the list of txt file

--- commune.py
+++ commune.py
@@ -1,32 +1,34 @@
 import os, glob 
+import shutil
 from os.path import basename
+from gtts import gTTS
 
 # now you can call it directly with basename
 #print(basename("/a/b/c.txt"))
 
-def get_list_txt_file(input_folder):
+def get_read_list_txt_file(input_folder):
 
-	list_text_file_name = []
+	readed_text_list = []
+	file_txt_name_list = []
 
 	os.chdir(input_folder)
 	for file in glob.glob("*.text"):
 		#print(file)
 		#print(basename(file))
 		file_text_name = os.path.splitext(file)[0]
-
-		list_text_file_name.append(file_text_name)
 		#print(file_text_name)
+		file = open(file_text_name+".text", "r") 
+		text = file.read() 
+		file_txt_name_list.append(file_text_name)
+		readed_text_list.append(text)
 
-	return list_text_file_name
+	return file_txt_name_list, readed_text_list
 
-
-
+'''
 if __name__ == '__main__':
 	input_folder= "./text_files"
-	print(get_list_txt_file(input_folder))
-	
-'''
-	#print(list_text_file_name)
+	file_txt_name_list, readed_text_list= get_read_list_txt_file(input_folder)
+	print(file_txt_name_list)
+	print(readed_text_list)'''
 
-for i in range(0,len(list_text_file_name)):
-	print(i) '''
+
